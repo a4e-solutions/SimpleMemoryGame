55cc5d94b1a6be5c328d311731f7caeb0f4beb6c
andre fourie <afourie.nz@gmail.com>
2024-11-04 14:54:48 +1300

    Initial Commit

---
+++ SimpleMemoryGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleMemoryGame
+//
+//  Created by andre fourie on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleMemoryGame/SimpleMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMemoryGameApp.swift
+//  SimpleMemoryGame
+//
+//  Created by andre fourie on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
